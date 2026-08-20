c16df2c54dc26125151b216e3b5072ba9d262825
SEBAS19380 <sd3023131933@gmail.com>
2026-08-20 17:32:24 -0500

    Add files via upload

---
+++ mesadeayuda/src/main/java/sd/proyecto/mesadeayuda/MesadeayudaApplication.java
@@ -0,0 +1,13 @@
+package sd.proyecto.mesadeayuda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MesadeayudaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MesadeayudaApplication.class, args);
+	}
+
+}
---
+++ mesadeayuda/src/test/java/sd/proyecto/mesadeayuda/MesadeayudaApplicationTests.java
@@ -0,0 +1,13 @@
+package sd.proyecto.mesadeayuda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MesadeayudaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
